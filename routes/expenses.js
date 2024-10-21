b2e0f58999b0177107d14350197ee84fba0b259e
vickyaltitude <vignvick3005>
2024-10-21 14:44:59 +0530

    fixed some bugs

--- routes/expenses.js
+++ routes/expenses.js
@@ -2,54 +2,37 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const ds = require('../util/data');
-const sequelize = require('../util/sequelize');
 require('dotenv').config();
 
-router.get('/',(req,res)=>{
-
+router.get('/', (req, res) => {
     const receivedhead = req.header("Authorization");
+    if (!receivedhead) {
+        return res.status(403).json({ message: 'No token provided.' });
+    }
+
     const token = receivedhead.split(' ')[1];
-    const user = jwt.verify(token,process.env.JWT_TOKEN_SECRET);
-   
-    ds.execute(`SELECT expense_details.id AS id,amount,description,category,user_id,created_date,name
-FROM expense_details
-INNER JOIN users ON expense_details.user_id = users.id
-WHERE users.id = ?;`,[user.userId]).then(resp =>{
-         let datas = resp[0];
-         res.json({datas})
-    }).catch(err => console.log(err));
-})
+    let user;
 
-router.post('/',async (req,res)=>{
-    let tran = await sequelize.transaction();
-    const receivedDat = req.body;
-    const receivedhead = req.header("Authorization");
-    const token = receivedhead.split(' ')[1];
-    const user = jwt.verify(token,process.env.JWT_TOKEN_SECRET);
-  
-     ds.execute('INSERT INTO `expense_details` (amount,description,category,user_id) VALUES(?,?,?,?)',[receivedDat.amnt,receivedDat.descr,receivedDat.catgry,user.userId],{transaction: tran}).then( async resp =>{
-        
-        ds.execute(`UPDATE users u
-                        SET total_expense = (
-                            SELECT SUM(e.amount)
-                            FROM expense_details e
-                            WHERE e.user_id = u.id
-                        )
-                        WHERE u.id = ?;`,[user.userId]).then(async resp => {
-                            await tran.commit()
-                            res.json({msg: 'Expense inserted successfully'});
+    try {
+        user = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid token.' });
+    }
 
-                        }).catch(async err => {
-                            await tran.rollback()
-                            console.log(err)
-                        })
+    if (!user || !user.userId) {
+        return res.status(401).json({ message: 'User not found.' });
+    }
 
-
-    }).catch(async err => {
-        await tran.rollback();
-        console.log(err)
-    }) 
-    
-})
+    ds.execute(`SELECT expense_details.id AS id, amount, description, category, user_id, created_date, name
+    FROM expense_details
+    INNER JOIN users ON expense_details.user_id = users.id
+    WHERE users.id = ?;`, [user.userId]).then(resp => {
+        let datas = resp[0];
+        res.json({ datas });
+    }).catch(err => {
+        console.error(err); 
+        res.status(500).json({ message: 'Database error occurred.' });
+    });
+});
 
 module.exports = router;
