f6bf7599327e55322c13aac23e552c6d35f952bd
vickyaltitude <vignvick3005>
2024-10-21 16:43:35 +0530

    fixed one bug in expenses route file

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,31 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const ds = require('../util/data');
-const sequelize = require('../util/sequelize');
+const ds = require('../model/data');
 require('dotenv').config();
 
-router.get('/',(req,res)=>{
-
+router.get('/', (req, res) => {
     const receivedhead = req.header("Authorization");
-    const token = receivedhead.split(' ')[1];
-    const user = jwt.verify(token,process.env.JWT_TOKEN_SECRET);
-   
-    ds.execute(`SELECT expense_details.id AS id,amount,description,category,user_id,created_date,name
-FROM expense_details
-INNER JOIN users ON expense_details.user_id = users.id
-WHERE users.id = ?;`,[user.userId]).then(resp =>{
-         let datas = resp[0];
-         res.json({datas})
-    }).catch(err => console.log(err));
-})
+    if (!receivedhead) {
+        return res.status(403).json({ message: 'No token provided.' });
+    }
 
-router.post('/',async (req,res)=>{
+    const token = receivedhead.split(' ')[1];
+    let user;
+    console.log(token)
+    try {
+        user = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid token.' });
+    }
+
+    if (!user || !user.userId) {
+        return res.status(401).json({ message: 'User not found.' });
+    }
+   
+    ds.execute(`SELECT expense_details.id AS id, amount, description, category, user_id, created_date, name
+    FROM expense_details
+    INNER JOIN users ON expense_details.user_id = users.id
+    WHERE users.id = ?;`, [user.userId]).then(resp => {
+        let datas = resp[0];
+        res.json({ datas });
+    }).catch(err => {
+        console.error(err); 
+        res.status(500).json({ message: 'Database error occurred.' });
+    });
+});
+
+app.post('/expenses',async (req,res)=>{
     let tran = await sequelize.transaction();
     const receivedDat = req.body;
     const receivedhead = req.header("Authorization");
     const token = receivedhead.split(' ')[1];
-    const user = jwt.verify(token,process.env.JWT_TOKEN_SECRET);
+    const user = jwt.verify(token,"mysecretcode");
   
      ds.execute('INSERT INTO `expense_details` (amount,description,category,user_id) VALUES(?,?,?,?)',[receivedDat.amnt,receivedDat.descr,receivedDat.catgry,user.userId],{transaction: tran}).then( async resp =>{
         
@@ -51,5 +66,6 @@ router.post('/',async (req,res)=>{
     }) 
     
 })
+
 
 module.exports = router;
